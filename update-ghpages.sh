859b0965523b22afae523ca2f73a021b8b1af09f
Bobby Warner <bobbywarner@gmail.com>
2016-01-13 09:30:26 -0600

    Fix error in build script

--- update-ghpages.sh
+++ update-ghpages.sh
@@ -23,7 +23,16 @@ LOCAL=$(git rev-parse @)
 REMOTE=$(git rev-parse @{u})
 BASE=$(git merge-base @ @{u})
 
-if [ "$LOCAL" = "$BASE" ]; then
+if [ "$LOCAL" = "$REMOTE" ]; then
+    git checkout -b gh-pages
+    npm install
+    rm -rf build
+    gulp clean build
+    git add build/ -f
+    git commit -m 'Deploying latest to GitHub pages'
+    git push -f origin gh-pages
+    git checkout master
+elif [ "$LOCAL" = "$BASE" ]; then
     echo "FAILED: Need to pull"
     exit 1
 elif [ "$REMOTE" = "$BASE" ]; then
@@ -33,16 +42,5 @@ else
     echo "FAILED: Diverged"
     exit 1
 fi
-
-git push origin --delete gh-pages
-
-git checkout -b gh-pages
-npm install
-rm -rf build
-gulp clean build
-git add build/ -f
-git commit -m 'Deploying latest to GitHub pages'
-git push -f origin gh-pages
-git checkout master
 
 echo "...Finished Grails 3 Plugins Deploy"
