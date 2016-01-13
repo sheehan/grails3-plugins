e80582e3c05288272a4991fa4a807fc5a77289e1
sheehan <mr.sheehan@gmail.com>
2016-01-12 22:21:57 -0600

    update build file

--- update-ghpages.sh
+++ update-ghpages.sh
@@ -1,13 +1,14 @@
-rm-rf build/gh-pages
+rm -rf build/gh-pages
 mkdir -p build/gh-pages
 cd build/gh-pages
 git clone git@github.com:sheehan/grails3-plugins.git
-git checkout gh-pages2
-git rebase master
+cd grails3-plugins
+git checkout gh-pages
+git reset --hard origin/master
 
 npm install
 gulp build
 
 git add build/ -f
 git commit -m "build files"
-git push
+git push -f
