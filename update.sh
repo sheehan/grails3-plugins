d4ee5a1c70f99d686012996e3147a0fea012836a
sheehan <mr.sheehan@gmail.com>
2016-01-14 22:32:48 -0600

    update script

--- update.sh
+++ update.sh
@@ -8,13 +8,27 @@
 #export TWITTER_ACCESS_TOKEN=x
 #export TWITTER_ACCESS_TOKEN_SECRET=x
 
-# TODO get latest master
+echo "Starting Grails 3 Plugins Update..."
+
+if ! git diff-index --quiet HEAD --; then
+    echo "FAILED: There are uncommited changes"
+    exit 1
+fi
+
+BRANCH=$(git symbolic-ref --short -q HEAD)
+
+if [ "$BRANCH" != "master" ]; then
+    echo "FAILED: Run on master branch"
+    exit 1
+fi
 
 groovy src/groovy/Fetch ./data/plugins.new.json
 groovy src/groovy/Compare ./data/plugins.json ./data/plugins.new.json
 
 mv ./data/plugins.new.json ./data/plugins.json
 
-# TODO commit & push updated data
+git add ./data/plugins.json
+git commit -m 'Updating plugin data'
+git push origin master
 
 ./update-ghpages.sh
